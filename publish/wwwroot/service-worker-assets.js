a9219ad0274388c4ecd57f3b94acd7523f558507
kosch104 <konstantin-schwarze@outlook.de>
2024-12-10 06:08:41 +0100

    Add publish files

---
+++ publish/wwwroot/service-worker-assets.js
@@ -0,0 +1,549 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-gvHfnndfEu1tRf0rFb5988rWq7ITIotOaE8+AMbKYbc=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-zLPXeNQlcFjOiHTxq90o\/aQv1TJK3xZIBQpEXrMM7QM=",
+      "url": "_framework\/dotnet.js"
+    },
+    {
+      "hash": "sha256-boyHe+0RMZDkzBKGhsrnXPhPg3oZ6UqJnTUzngJPjNY=",
+      "url": "_framework\/dotnet.native.8.0.5.dulydo12xc.js"
+    },
+    {
+      "hash": "sha256-b3QtgAqvT5FoNyYgoEYsdE4zF5C1Is6lhQ8VEkROor8=",
+      "url": "_framework\/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-5oi05+Ql\/1JzyAphppAHmbRquli4a3\/0XLMa8mQDKn8=",
+      "url": "_framework\/dotnet.runtime.8.0.5.heimwzhjiu.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37\/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-Xn1ks6j1AIE7oCgY4iWkJQZpGT8rBz3A52yGENLrjUQ=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-8XB\/r3VJSHjLn1XSXYd87i08R8k3aY9KMxsM202R\/uM=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-7cBhJ78YkXBN\/eLA2hozeHiNZ8SsBoWaB3uH7bwT4V0=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-IKQWyOFxNGQEBGGGvN9kyacH9qSKTZNHcunT31UeXY8=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-87sn2TYqgdZ95sXmavjKEzoEfMgHnYQ9LOvnMX+aZcI=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-Sxmy2ZS134URxbHEvdbS6NcQ3zXS7UWx\/5ZPpwiW7FA=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-jYqHUZ07UYWc8POk7xhas6xQYH7t1qoTcylqoDqncJk=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-Q6Yhy+74Ao+9QtddrcQ0vjhvbm2rxNl7JVAtSduGoh0=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-gg8xZqJsBBrrNEyUGzYqhL3sqkuZ4AHAvdTdL9nZ0S0=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-LcaPEaPNm7pFT9kZyDHfXsV4Z9cGFGu7KFdL3o3UQks=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-8BH+kQfjYuZWxprOICXJ4+tU0OdJOYDKN7G0S3zYYHI=",
+      "url": "_framework\/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-ezKJDNjeghS\/qMdQmUpBPdH7bCDLrqkNqS6RsLxlpOY=",
+      "url": "_framework\/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-6187ynEahDlSLMBvD4vAmiLpZ3clRb5xu6rM7O8AxNo=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-psv4A3pBPmn3fv9Mf8Yv5HDErRoCTHh7L+3cXHL+IZ4=",
+      "url": "_framework\/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-ZyIqQss3I64BQC2gc7JEDrkYq14vXKEXuw5vBJ5oFYM=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-hcJzzCFysdsZwkKtaDyF6EsGEfMnqT9u7dGZXk0jTMc=",
+      "url": "_framework\/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-C2yGj25f\/FtCzhXb8uSBgKh0rVIN3sNF8iYNyK3qTrc=",
+      "url": "_framework\/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-RLeTQOOEEy4QXGeQLyLNY8k+aaF4W\/EDE4Gx\/TW+zxY=",
+      "url": "_framework\/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-1Jllf2ZXiKDXRLYWXiSV+foHQKQ6pHFD+Ay7SuBgBfk=",
+      "url": "_framework\/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-PhWuJsnPolBvHLRVNAGqwS6T8LCPm8n6pPha87lQG04=",
+      "url": "_framework\/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-v39hSGx8CsUgAqL3NRWe3trI4NAE178m9StCanXndjY=",
+      "url": "_framework\/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-9HUy4gMJdNTLo7N0VwoI8QasP0gpyKztKnHbnQcS3e8=",
+      "url": "_framework\/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-M3oLP1NWDcEnj1xIRy3T3iCFq8RbTAjFilsMBhSoLz8=",
+      "url": "_framework\/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-pe2ts7Re+Rwt6KzGosBQFy7487Iz3gRoalehJ\/NzCaE=",
+      "url": "_framework\/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-L5koUySbtCXC2Wr8ZsCQ1aLNwj8rYddJ4ey4cJgdvJs=",
+      "url": "_framework\/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-3H19jIHJ4w+WUW0Uqv97fJSN3pQgpds4Xgb9oDBfmUU=",
+      "url": "_framework\/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-sNaVmHWh4d4fAAq8MnXbN95X6doWzK99NK7E6wghB1Q=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-+U+5LNjPpnY6WZEh6e3Yyn8Zi7AMEeVF1C9DvmbRbcM=",
+      "url": "_framework\/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-V69v6JYAgTsrgM4epAyn\/+1+KjM60Hxzokb2hKtbPt0=",
+      "url": "_framework\/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-n42G6dtH8ZLNiGlM75idHL7Ltdau1YP1Bmhkkx3WSPc=",
+      "url": "_framework\/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-o6Ef\/uOI9sFxJXKnXQbJnRMbcIIJ\/ZFdc4fFqA9wu7Y=",
+      "url": "_framework\/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-5dwYGu9k8oPMgI0WTZ33k6KNnEGW8dQMZCV8x3S1s6M=",
+      "url": "_framework\/VariationPackCreator.wasm"
+    },
+    {
+      "hash": "sha256-ZkkFIKTZ4Mk616gW5BZiBLKUx1KJLgp9BnIhxMbRN3k=",
+      "url": "VariationPackCreator.styles.css"
+    },
+    {
+      "hash": "sha256-FpcEudcKgvG0CUbSrnkW7Shm0hjpo7XG6jeu\/AVfs4g=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-SiIVMGgRhdXjKSTIddX7mh9IbOXVcwQWc7\/p4nS6D\/0=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-4mWsDy3aHl36ZbGt8zByK7Pvd4kRUoNgTYzRnwmPHwg=",
+      "url": "favicon.png"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-2s12DuSxm+bgWfvnWYUkNu9UzUjx+Uo1LQ9\/i0QCgqw=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-ZSjO21WQJtBXcGqSrBVXbQ8ouVwPRQXdyGRUybj+GWo=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-gppMUSL\/46EOhEr+Qz1e\/xyjKcqBnXETcZNwppZdBdE=",
+      "url": "presets\/German Vehicle Pack.json"
+    },
+    {
+      "hash": "sha256-kIKYWi4BGVENynqwGwa5w9o\/cwRt0tz35w7SjXI9zm0=",
+      "url": "presets\/Realistic Global.json"
+    },
+    {
+      "hash": "sha256-mwk34eHQFYYpS7BSEovmhGXP3CwxhAXb15DlqjrbQYs=",
+      "url": "presets\/Vancouver, Canada.json"
+    },
+    {
+      "hash": "sha256-bJqIKNiynowZXt67es+4loiTmx9LVA\/wIsHv2wiI+l8=",
+      "url": "presets\/Vanilla Rebalance.json"
+    },
+    {
+      "hash": "sha256-AQ+EFwN+VDCIN54kDhjMthJ2YYEHjzpCKB5PA\/D6vAY=",
+      "url": "presets\/Vanilla.json"
+    },
+    {
+      "hash": "sha256-8IXAYC6ZBZV0JnG5EVw7d0\/Gm4eITpH\/HHqKPkZSojg=",
+      "url": "thumbnails\/AdministrationVehicle01.png"
+    },
+    {
+      "hash": "sha256-iSsC2+yMPAzOFml0GqhpyLFISlU5Kxd3ATL+j43qKkI=",
+      "url": "thumbnails\/BlastholeDrillingRig01.png"
+    },
+    {
+      "hash": "sha256-fwuJSIT+n5XCSAvP+Ur0LoOIBQWzdLWype76XJ+vivo=",
+      "url": "thumbnails\/Bus01.png"
+    },
+    {
+      "hash": "sha256-vnEtd1mWUTXkdfW1VaCR6dS7SgdIlIy3nCmSCAnVGJU=",
+      "url": "thumbnails\/Bus02.png"
+    },
+    {
+      "hash": "sha256-HdoNfDanicodL8ndRKs69+lGqZbB5rIeaQQlsYwY454=",
+      "url": "thumbnails\/Bus03.png"
+    },
+    {
+      "hash": "sha256-Ljr34FCn1oroouTMaZU+iN6n2NWEB3I4Lgs\/xpb1bEk=",
+      "url": "thumbnails\/BusCO01.png"
+    },
+    {
+      "hash": "sha256-7o7Y03FvkPwVTUNBFvCPJOHmAa4\/OGV9EoGjiH\/E91M=",
+      "url": "thumbnails\/BusCO02.png"
+    },
+    {
+      "hash": "sha256-xE3fTxrfBhlE4jrULw9Ffz05CxYsPfIC36mM2pNUbdQ=",
+      "url": "thumbnails\/CamperTrailer01.png"
+    },
+    {
+      "hash": "sha256-K+xgKIAh+8os5COgh0GtvpCqzIqwmzVTcJjM2YKXPJ4=",
+      "url": "thumbnails\/Car01.png"
+    },
+    {
+      "hash": "sha256-gyMN0ghACIgnF6RlYjrw3yFSEYmXxEliwFZ4SkoOO10=",
+      "url": "thumbnails\/Car02.png"
+    },
+    {
+      "hash": "sha256-JcIqCCsY4YKtlUQ3uMe6t718uNbaPxfuPekAUI8NnWg=",
+      "url": "thumbnails\/Car03.png"
+    },
+    {
+      "hash": "sha256-XDPKQV3dPLP7cpNcTL\/0\/KxJKA3w1+WXjg53A7uKe9k=",
+      "url": "thumbnails\/Car04.png"
+    },
+    {
+      "hash": "sha256-Mf5sk89mKe5pYmdCPoo7KARxyf4bcqGfUQMBXso7XGg=",
+      "url": "thumbnails\/Car05.png"
+    },
+    {
+      "hash": "sha256-x+lX3j0CoctMPx5\/yqpCqxj8iu0P0bum2DJRmz2xaNo=",
+      "url": "thumbnails\/Car06.png"
+    },
+    {
+      "hash": "sha256-C1nv\/Jm1Z76QkqtDREWp6uXdelnCBjpkhZRSL8d1ma0=",
+      "url": "thumbnails\/Car07.png"
+    },
+    {
+      "hash": "sha256-Dmpj7p5XhFzhamw238iG0M+sTinRjKQ\/ObA7GSkDGtI=",
+      "url": "thumbnails\/Car08.png"
+    },
+    {
+      "hash": "sha256-rDPkdHSKmiXHLiQWNJ8u6h8UzOUTZqxaHtoxaf07x60=",
+      "url": "thumbnails\/Car09.png"
+    },
+    {
+      "hash": "sha256-GFhlkVmm\/6KPvt2JFpT5U5hrVEcYwswgptSypS7alHE=",
+      "url": "thumbnails\/CoalTruck01.png"
+    },
+    {
+      "hash": "sha256-yIz8wIZ8XSTLGF5wiEOwFIUG0\/U9crN4lZiXy4Bc8Xk=",
+      "url": "thumbnails\/CombineHarvester01.png"
+    },
+    {
+      "hash": "sha256-++KvnXM\/i8d3QJXQePeOrIQmMgzI6m03x19XhJ4veqE=",
+      "url": "thumbnails\/EU_Ambulance01.png"
+    },
+    {
+      "hash": "sha256-MzR4KM\/YBm8Pj7zsQe7qMtIQWwEQ8mkvRRDIAry1SPw=",
+      "url": "thumbnails\/EU_DeliveryVan01.png"
+    },
+    {
+      "hash": "sha256-87VPwCnuN\/Kk3hC6yT4ZW\/TRtyUBbpyu4yxYPWUlSzE=",
+      "url": "thumbnails\/EU_FireTruck01.png"
+    },
+    {
+      "hash": "sha256-GytWfmTwta8qYPG4Ov64K14mP1Q5usIx15Nuhfgkla0=",
+      "url": "thumbnails\/EU_PoliceVehicle01.png"
+    },
+    {
+      "hash": "sha256-KM60ZgdAett9KzgHt\/MIgRUI07pXqsunRUN8O6UG+as=",
+      "url": "thumbnails\/EU_PoliceVehicle02.png"
+    },
+    {
+      "hash": "sha256-wBPt3XSdnQIz\/YUF186NeCHDZ+Zrvs2nBb+ryZ1l2eE=",
+      "url": "thumbnails\/EU_PostVan01.png"
+    },
+    {
+      "hash": "sha256-kXxjkQQN4BzospTcHuB1duFMDTWdCHkMzath\/XNXJK8=",
+      "url": "thumbnails\/EU_TruckTractor01.png"
+    },
+    {
+      "hash": "sha256-T3l+L9tuyBe2awdEALX7iegbS2iLfhJ+Mp2Ns7ytN3s=",
+      "url": "thumbnails\/ForestForwarder01.png"
+    },
+    {
+      "hash": "sha256-a9e6ZLkmR\/Z0R8u9DLMZHxe7qIORQy0\/wuEtm6DjcF8=",
+      "url": "thumbnails\/ForestForwarderTrailer01.png"
+    },
+    {
+      "hash": "sha256-rlVclVkhbmevUF4TRSKiVxaqzBMPKqirsoFMIuZ6o7Y=",
+      "url": "thumbnails\/ForestHarvester01.png"
+    },
+    {
+      "hash": "sha256-YUuQK4GZOILyXtX+UlraRo4YEdEEA0rMNugDz5Vzewc=",
+      "url": "thumbnails\/ForestHarvesterTrailer01.png"
+    },
+    {
+      "hash": "sha256-jiv1hUNtbdnHg94SjVPTNoz6rrxVkZfeX+3aAOe6Dy4=",
+      "url": "thumbnails\/FrontendLoader01.png"
+    },
+    {
+      "hash": "sha256-RgFsdWV49gjGhG7wKulndVlJjCTyf6YTT4jR+YuO5rs=",
+      "url": "thumbnails\/FrontendLoaderTrailer01.png"
+    },
+    {
+      "hash": "sha256-G4LLQ2WkVfMOQsqlhnmlU5scTbNnptwuHk6+d1qZmGw=",
+      "url": "thumbnails\/GarbageTruck01.png"
+    },
+    {
+      "hash": "sha256-VY694RMkaemTkxSTgjiNxudIt2l0FlV5gdtOSj57BwA=",
+      "url": "thumbnails\/Hearse01.png"
+    },
+    {
+      "hash": "sha256-r\/\/BPnqG9jVtAQiujR79LWQFRLSdI7HaDXEgdYqKfPU=",
+      "url": "thumbnails\/HelicopterFire01.png"
+    },
+    {
+      "hash": "sha256-jW3NXJJ8rWasnuygr0I5zj1QIhzmZnH4AxEkLrxzjxs=",
+      "url": "thumbnails\/HelicopterHealthcare01.png"
+    },
+    {
+      "hash": "sha256-lTi56Ie+89N6j5otxZkE9K6q\/xTbElVDl9zBtPx3iZI=",
+      "url": "thumbnails\/HelicopterPassenger01.png"
+    },
+    {
+      "hash": "sha256-hNR\/KWqk+irNrZ4s\/gl\/vSeM03y525KEgDNMbCF9rmM=",
+      "url": "thumbnails\/HelicopterPolice01.png"
+    },
+    {
+      "hash": "sha256-2IEdRviu+\/HAGNXNm4xRAUvR+MUUZh45DMdcec5qGj8=",
+      "url": "thumbnails\/JunkCar01.png"
+    },
+    {
+      "hash": "sha256-ZRWR+sBn8LErUFp42T\/KSThhR1QuPxnztXw9byIVkuE=",
+      "url": "thumbnails\/JunkCar02.png"
+    },
+    {
+      "hash": "sha256-sAyGV\/WhGHPmzi3o\/SOWTDQiTLcuScIq5+N8w2pSW6U=",
+      "url": "thumbnails\/JunkCar03.png"
+    },
+    {
+      "hash": "sha256-8suiENBi4Ixk0dRsGS4RfuLfjl1CsMjSSVpoEeu1QhQ=",
+      "url": "thumbnails\/JunkCar04.png"
+    },
+    {
+      "hash": "sha256-m9KfHHci8wR5rDbUzVig1eexCBFyjme4FvjSqOTT1u0=",
+      "url": "thumbnails\/JunkCar05.png"
+    },
+    {
+      "hash": "sha256-xZ5wORWLM8IF0Uh3JSR7TZdVA916\/9QAjL8tRoz4\/WA=",
+      "url": "thumbnails\/MiningExcavator01.png"
+    },
+    {
+      "hash": "sha256-6s10khQSxBFFVy0VzKy5zyxCAxg3PZwY7TP\/W6MB6zo=",
+      "url": "thumbnails\/Motorbike01.png"
+    },
+    {
+      "hash": "sha256-\/uRIgsa+0cTjcf3FhqZD0UkL4cGh0PLc\/pAptmU3WT4=",
+      "url": "thumbnails\/MotorbikeDelivery01.png"
+    },
+    {
+      "hash": "sha256-Y947umdlnrFor5130OQOAYr5ugI9BxH94tNzOMROEes=",
+      "url": "thumbnails\/MuscleCar01.png"
+    },
+    {
+      "hash": "sha256-1O9pROSxHXe1bNLNPC8OXir4kookSplFY\/MwAXG3f+o=",
+      "url": "thumbnails\/MuscleCar02.png"
+    },
+    {
+      "hash": "sha256-wTZ8AC8MoNRBxbA1HkesG1wHw1GrYBJtijpniX12HfY=",
+      "url": "thumbnails\/MuscleCar03.png"
+    },
+    {
+      "hash": "sha256-PnC5r5mPP6ymBPy6Isf0vP4gGh8Q1HzDKgv+DkoNUEw=",
+      "url": "thumbnails\/MuscleCar04.png"
+    },
+    {
+      "hash": "sha256-aEUZm2dJWvHQvm0tMUu+fDmuzt2xT2XTUGqmxFVWRfU=",
+      "url": "thumbnails\/MuscleCar05.png"
+    },
+    {
+      "hash": "sha256-xAcFH9aDqoMLEYsXcwSgSXFPcTVZMgVHweS3yvzfJl8=",
+      "url": "thumbnails\/NA_Ambulance01.png"
+    },
+    {
+      "hash": "sha256-si4xnpl2MJehZVLnqeHE0LZhP6AxjVsK7+xz90oBGTY=",
+      "url": "thumbnails\/NA_DeliveryVan01.png"
+    },
+    {
+      "hash": "sha256-48QvAh18bAmaDeLxBLzDUw5pXHWpE6VPCAueKnqqgBc=",
+      "url": "thumbnails\/NA_FireTruck01.png"
+    },
+    {
+      "hash": "sha256-a4pzgtPkd8DbSafRontk579Es+iESN\/LM0fWaevqYvo=",
+      "url": "thumbnails\/NA_PoliceVehicle01.png"
+    },
+    {
+      "hash": "sha256-7QlJ\/oj2sbdt+b87pfViCXvNLJt\/cKEynvDpHXuj+L4=",
+      "url": "thumbnails\/NA_PoliceVehicle02.png"
+    },
+    {
+      "hash": "sha256-EDuTO\/PklfWNv3jsuFVbiiy9DAIec5fyN9+OgGd1Cco=",
+      "url": "thumbnails\/NA_PostVan01.png"
+    },
+    {
+      "hash": "sha256-8QmP339Yw\/+QpqB1FSOHp+VUebOxstagtmU9OzBDvlI=",
+      "url": "thumbnails\/NA_TruckTractor01.png"
+    },
+    {
+      "hash": "sha256-NDHTccyfRtDEcwPnBY1kG1wYXp+a22AS4zkaad233JU=",
+      "url": "thumbnails\/OilTruck01.png"
+    },
+    {
+      "hash": "sha256-659\/xDsLyUF+ynWSE\/obeoe7rBPCPqWPqMpsCEi5lUs=",
+      "url": "thumbnails\/OreMiningTractor01.png"
+    },
+    {
+      "hash": "sha256-poHJ6NalD+Cc5HE1OidTepgqxSC4PiwXvLcHUk5uDME=",
+      "url": "thumbnails\/OreMiningTractorTrailer01.png"
+    },
+    {
+      "hash": "sha256-fe9uCR2YauvxjRRC0ZLtawpZ1jVExRhfPZtcwech9WE=",
+      "url": "thumbnails\/OreMiningTruck01.png"
+    },
+    {
+      "hash": "sha256-Eal7aU2Otb3HTlWqU+6bdeinqV9Hfvv\/WJcT\/3bFWTg=",
+      "url": "thumbnails\/ParkMaintenanceVehicle01.png"
+    },
+    {
+      "hash": "sha256-0AdkhqRYp\/VH7mDnviLKydZ6AGow5EBOJkb2hfRx61c=",
+      "url": "thumbnails\/PrisonVan01.png"
+    },
+    {
+      "hash": "sha256-A\/YHc+N\/tU84JW3zyWhaD7JC+UA8y98i3x0liQdT1hM=",
+      "url": "thumbnails\/RoadMaintenanceVehicle01.png"
+    },
+    {
+      "hash": "sha256-bctEhp1pyzFdd7WKOYbrRxf9AMdoqJnCUwYCcQ1cdXc=",
+      "url": "thumbnails\/Scooter01.png"
+    },
+    {
+      "hash": "sha256-9Bwo7ObXq4apeNPx31YtAlA6SXa745mn\/Lzvh7Vng9A=",
+      "url": "thumbnails\/Snowplow01.png"
+    },
+    {
+      "hash": "sha256-iJe0QENb\/1ZtjIFzSAwSS3Dshi72Ar+jZwVEjTDSc6U=",
+      "url": "thumbnails\/Taxi01.png"
+    },
+    {
+      "hash": "sha256-b3UgjrVaD3gRKT73q0FMgNy5Jkpv\/hoMP6VZsVtoL8A=",
+      "url": "thumbnails\/Taxi02.png"
+    },
+    {
+      "hash": "sha256-Y+x3CikZgK5fxcVaFbdw4CbSMXkoIbHx7ks2WRpJiQg=",
+      "url": "thumbnails\/Tractor01.png"
+    },
+    {
+      "hash": "sha256-EZQns35kA\/FjRE79+NCgBbQeG8x1e8KRhXZsCIkaFYE=",
+      "url": "thumbnails\/TractorFertilizerSpreader01.png"
+    },
+    {
+      "hash": "sha256-i8cRIZ97Pz3qRJelrfaHoELalqhfx8mosV1yqgnyTBI=",
+      "url": "thumbnails\/TractorPlough01.png"
+    },
+    {
+      "hash": "sha256-+q4uB4mOJVOx8wztB8C8VmJpn1prId2fw\/gfF2JkcP4=",
+      "url": "thumbnails\/TractorSowingMachine01.png"
+    },
+    {
+      "hash": "sha256-IPZ2OzRC3RVg9e+e3LpPKu5OYlzQH9s06NesSQUXx9k=",
+      "url": "thumbnails\/TractorSprayer01.png"
+    },
+    {
+      "hash": "sha256-vJAYA0OaqzSD\/yS41rROgtLyTEx\/ie4SFZTccJRlz5A=",
+      "url": "thumbnails\/TractorTrailer01.png"
+    },
+    {
+      "hash": "sha256-uFs4ba+SldYKQWg8Vmd05vctwOYAIGX58VHgKMMxIXc=",
+      "url": "thumbnails\/TractorTrailer02.png"
+    },
+    {
+      "hash": "sha256-2fNOcXHRyghD2u0FQsAujpnIGPzlEFMzJy7UYGFgLHw=",
+      "url": "thumbnails\/TractorTrailer03.png"
+    },
+    {
+      "hash": "sha256-pr\/E8NM5EEAN0njfvqi7vFMhVRPJYdCTHko2Ovj8UJM=",
+      "url": "thumbnails\/TruckTrailer01.png"
+    },
+    {
+      "hash": "sha256-109NIeCME4Ywk5w50NlXxYsD4hpihJ9Z+sMn1JGC5pI=",
+      "url": "thumbnails\/TruckTrailer02.png"
+    },
+    {
+      "hash": "sha256-y+rGsKQpP0n9l8iOvJWT69WUSM2QlheNFFpT49u9znw=",
+      "url": "thumbnails\/TruckTrailer03.png"
+    },
+    {
+      "hash": "sha256-+PqbwzM+B6y\/1UeV4qqM8OV5j7UJHAea9n\/lKSXCYzw=",
+      "url": "thumbnails\/TruckTrailer04.png"
+    },
+    {
+      "hash": "sha256-slR1C24XnBRtUGcsZPZvWA8ZdbWfouwiGR2eOWq9tzs=",
+      "url": "thumbnails\/Van01.png"
+    }
+  ],
+  "version": "fewnHZQz"
+};
