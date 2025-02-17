fcc52a363c48c7b46f3733fe39dd4aadb01ed892
Mimonsi <konstantin-schwarze@outlook.de>
2025-02-17 01:38:07 +0100

    Add Telemetry

---
+++ Telemetry.cs
@@ -0,0 +1,28 @@
+﻿namespace VariationPackCreator;
+
+public static class Telemetry
+{
+    private static HttpClient _httpClient;
+    private static string baseUrl = "http://telemetry.mimonsi.de:5005";
+    
+    static Telemetry()
+    {
+        _httpClient = new HttpClient();
+    }
+    
+    public static void EnterWebsite()
+    { 
+        _httpClient.GetAsync(baseUrl + "/enterWebsite").ConfigureAwait(false);
+    }
+
+    
+    public static void DownloadPack(string name)
+    {
+        _httpClient.GetAsync(baseUrl + "/downloadPack?name=" + name);
+    }
+
+    public static void StartWithTemplate(string template)
+    {
+        _httpClient.GetAsync(baseUrl + "/startWithTemplate?template=" + template);
+    }
+}
